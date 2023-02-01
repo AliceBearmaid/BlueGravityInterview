fe6ec981e4e4b3ae09f121c1ec4d8112d940bbd4
AliceBearmaid <31712870+AliceBearmaid@users.noreply.github.com>
2023-02-01 16:38:27 -0600

    Player & Demo Scene
    Player now has a Prefab, animator, animations, and controlling script.

---
+++ Assets/_BlueGravity/Scripts/Player/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] Rigidbody2D rbody;
+    [SerializeField] Animator anmtr;
+    [Header("Movement")]
+    public bool canMove;
+    [SerializeField] float speed;
+    [Header("Inputs")]
+    [SerializeField] Vector2 inputSpeed;
+    [SerializeField] Vector2 lastInputSpeed;
+
+    #region Unity Methods
+    private void Start()
+    {
+        SetUp();
+    }
+    private void Update()
+    {
+        InputHandling();
+        AnimationHandling();
+    }
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Movement();
+        }
+    }
+
+   
+    #endregion
+
+    void SetUp()
+    {
+        canMove = true;
+    }
+   
+    void InputHandling()
+    {
+        inputSpeed = Vector2.up * Input.GetAxisRaw("Vertical") + Vector2.right * Input.GetAxisRaw("Horizontal");
+        if(inputSpeed.x !=0 || inputSpeed.y != 0)
+        {
+            lastInputSpeed = inputSpeed;
+        }
+    }
+
+    void Movement()
+    {
+        rbody.velocity = inputSpeed.normalized*speed;
+    }
+
+   public void AnimationHandling()
+    {
+        anmtr.SetFloat("SpeedX",inputSpeed.x);
+        anmtr.SetFloat("SpeedY", inputSpeed.y);
+        anmtr.SetFloat("LastMovingSpeedX", lastInputSpeed.x);
+        anmtr.SetFloat("LastMovingSpeedY", lastInputSpeed.y);
+        anmtr.SetBool("IsMoving", inputSpeed.magnitude > 0);
+    }
+
+}
